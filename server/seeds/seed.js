301915f51a91b5f03818149e3b30e49f289f8c15
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-06 10:35:59 -0600

    Modified Seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,40 @@
 const db = require('../config/connection');
-const { Tech } = require('../models');
+const { User, Piece, Category, Collection } = require('../models');
 
-const techData = require('./techData.json');
+const userData = require('./userData.json');
+const pieceData = require('./pieceData.json');
+const categoryData = require('./categoryData.json');
+const collectionData = require('./collectionData.json');
 
 db.once('open', async () => {
-  await Tech.deleteMany({});
+  // clean database
+  await user.deleteMany({});
+  await piece.deleteMany({});
+  await category.deleteMany({});
+  await collection.deleteMany({});
 
-  const technologies = await Tech.insertMany(techData);
+  // bulk create each model
+  const user = await user.insertMany(userData);
+  const piece = await piece.insertMany(pieceData);
+  const category = await category.insertMany(categoryData);
+  const collection = await collection.insertMany(collectionData);
 
-  console.log('Technologies seeded!');
+  for (newClass of classes) {
+    // randomly add each class to a school
+    const tempSchool = schools[Math.floor(Math.random() * schools.length)];
+    tempSchool.classes.push(newClass._id);
+    await tempSchool.save();
+
+    // randomly add a professor to each class
+    const tempProfessor = professors[Math.floor(Math.random() * professors.length)];
+    newClass.professor = tempProfessor._id;
+    await newClass.save();
+
+    // reference class on professor model, too
+    tempProfessor.classes.push(newClass._id);
+    await tempProfessor.save();
+  }
+
+  console.log('all done!');
   process.exit(0);
 });
