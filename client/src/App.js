2441dac79f0fb8910b21c50172b0ecec8db66e28
HunterLM <hunter109@gmail.com>
2022-06-06 13:20:34 -0500

    added navbar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-// import logo from './logo.svg';
-// import './App.css';
-import Footer  from "./components/Footer";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-
-    <div>
-    <h2>test</h2>
-    <Home />
-    <Footer />
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MuiNabBar from "./components/navbar/index";
 
 function App() {
   return (
     <div>
+      <MuiNabBar />
       <Home />
->>>>>>> 1caf6469cc431e5ca5a9bb2da11b1e8a9404ef80
     </div>
     
   );
