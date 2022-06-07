5d38c9008a3cd3dce76158bdd2fd4233f4cdcae5
KnightNight <gilbenvenitz18@gmail.com>
2022-06-07 10:20:29 -0500

    switch

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,72 @@
 import React from 'react';
 import test from './images/test.jpg';
 import './css/Home.css';
-
 // Home page to give them a little taste of what the site is before they sign up
 
 // if clicked on a piece or collection without being signed in, push to login/signup page
 
+const styles = {
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    size: 'is-one-quarter',
+    marginTop: '5%',
+    marginRight: '2.5%',
+  },
+};
+
 function Home() {
-    return(
-        
-        <div className="container" id="">
+  return (
+    <div style={styles.mainContainer} className="container" id="">
+      {/* row for search  */}
+      <div className="row" id=""></div>
+      {/* Gallery */}
+      <section style={styles.cardContainer} className="columns" id="">
+        <div style={styles.card} className="card column " id="">
+          <div className="card-title">
+            <h4>Artist</h4>
+          </div>
+          <div className="card-image" id="">
+            <img className="image" src={test} alt="test"></img>
+          </div>
+        </div>
 
-            {/* row for search  */}
-            <div className="row" id="">
+        <div style={styles.card} className="card column " id="">
+          <div className="card-title">
+            <h4>Artist</h4>
+          </div>
+          <div className="card-image" id="">
+            <img className="image" src={test} alt="test"></img>
+          </div>
+        </div>
+        <div style={styles.card} className="card column " id="">
+          <div className="card-title">
+            <h4>Artist</h4>
+          </div>
+          <div className="card-image" id="">
+            <img className="image" src={test} alt="test"></img>
+          </div>
+        </div>
+        <div style={styles.card} className="card column " id="">
+          <div className="card-title">
+            <h4>Artist</h4>
+          </div>
+          <div className="card-image" id="">
+            <img className="image" src={test} alt="test"></img>
+          </div>
+        </div>
 
-
-            </div>
-
-            {/* Gallery */}
-            <div className="columns" id="">
-        
-                    <div className="card column " id="">
-                        <div className="card-title">
-                            <h4>Artist</h4>
-                        </div>
-                        <div className="card-image" id="">
-                        <img className="image" src={test} alt="test"></img>
-                        </div>
-                    </div>
-
-                    <div className="card column " id="">
-                        <div className="card-title">
-                            <h4>Artist</h4>
-                        </div>
-                        <div className="card-image" id="">
-                        <img className="image" src={test} alt="test"></img>
-                        </div>
-                    </div>
-
-                    {/* <div className="card column " id="">
+        {/* <div className="card column " id="">
                         <div className="card-title">
                             <h4>Artist</h4>
                         </div>
@@ -46,29 +74,8 @@ function Home() {
                         <img className="image" src={test} alt="test"></img>
                         </div>
                     </div> */}
-
-            </div>
-
-      {/* Gallery */}
-      <section className="row m3" id="">
-        <div className="card col s1 m4 l4" id="">
-          <div className="card-title">
-            <h5>Artist</h5>
-          </div>
-          <div className="card-image" id="">
-            <img className="image" src={test} alt="test"></img>
-          </div>
-        </div>
-
-        <div className="card col s1 m4 l4" id="">
-          <div className="card-title">
-            <h5>Artist</h5>
-          </div>
-          <div className="card-image" id="">
-            <img className="image" src={test} alt="test"></img>
-          </div>
-        </div>
       </section>
+      {/* Gallery */}
     </div>
   );
 }
