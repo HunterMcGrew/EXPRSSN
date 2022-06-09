b2ad1d735328d2eacbba4dd3e784837adb9f0637
HunterLM <hunter109@gmail.com>
2022-06-09 14:01:43 -0500

    trying to fix collection-single

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import test from './images/test.jpg';
 import './css/Home.css';
+
 // Home page to give them a little taste of what the site is before they sign up
 
 // if clicked on a piece or collection without being signed in, push to login/signup page
