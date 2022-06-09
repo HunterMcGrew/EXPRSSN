b4a27dae476e40de547c59fe808647bade8c2160
Alex Ortega <alexdiclothingco@icloud.com>
2022-06-09 10:56:24 -0600

    updated front

--- client/src/App.js
+++ client/src/App.js
@@ -11,31 +11,22 @@ import SignUp from './pages/SignUp';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import MobileNav from './components/MobileNav';
-<<<<<<< HEAD
 import Artists from './pages/All-Artist';
-import Collections from './pages/All-Collections';
-import SinglePiece from "./pages/Single-Piece";
+import About from './pages/About';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
-});
+})
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
+  return{
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
-    },
+   },
   };
 });
-=======
-import Artists from './pages/All-Arists';
-import About from './pages/About';
->>>>>>> Front
 
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
@@ -59,14 +50,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-          <Route path="/explore" element={<Collections />} />
-
-          <Route path="/single-piece" element={<SinglePiece />} />
-=======
           <Route path="/artists" element={<Artists />} />
           <Route path="/about" element={<About />} />
->>>>>>> Front
 
           <Route path="*" element={<NotFound />} />
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -15,11 +15,6 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SignUp from "./SignUp";
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-<<<<<<< HEAD
-
-import Auth from '../utils/auth';
-=======
->>>>>>> Front
 
 import Auth from '../utils/auth';
 
@@ -39,30 +34,30 @@ const Login = (props) => {
     });
   };
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      console.log(formState);
-     
-      try {
-        console.log('we trying')
-        const { data } = await login({
-          variables: { ...formState },
-        });
-        console.log('we tried')
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
 
-  
-        Auth.login(data.login.token);
-      } catch (error) {
-        console.error(error.message);
-      }
-    
-  
-      // clear form values
-      setFormState({
-        email: '',
-        password: '',
+    try {
+      console.log('we trying')
+      const { data } = await login({
+        variables: { ...formState },
       });
-    };
+      console.log('we tried')
+
+
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error.message);
+    }
+
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -80,101 +75,82 @@ const Login = (props) => {
           <Avatar sx={{ m: 1, bgcolor: "black" }}>
             <LockOutlinedIcon />
           </Avatar>
-      
+
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
           {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={formState.email}
-<<<<<<< HEAD
-              onChange={handleChange}
-=======
-                  onChange={handleChange}
->>>>>>> Front
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={formState.password}
-<<<<<<< HEAD
-              onChange={handleChange}
-=======
-                  onChange={handleChange}
->>>>>>> Front
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-<<<<<<< HEAD
-<<<<<<< HEAD
-              sx={{ mt: 3, mb: 2, bgcolor: 'black' }}
-=======
-              sx={{ mt: 3, mb: 2, bgcolor: "black" }}
->>>>>>> 243600ca7a941771b559d1d56de8aed1d09dfbdf
-=======
-              sx={{ mt: 3, mb: 2, bgcolor: "black" }}
->>>>>>> Front
+            <p>
+              Success! You may now head{' '}
+              <Link to="/">back to the homepage.</Link>
+            </p>
+          ) : (
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              Sign In
-            </Button>
-            
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, bgcolor: 'black' }}
+              >
+                Sign In
+              </Button>
+
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="./SignUp" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="./SignUp" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box> )}
+            </Box>)}
           {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+            <div className="my-3 p-3 bg-danger text-white">
+              {error.message}
+            </div>
+          )}
         </Box>
       </Container>
     </ThemeProvider>
   );
 }
 
-<<<<<<< HEAD
 export default Login;
-=======
-export default Login
->>>>>>> Front
+
