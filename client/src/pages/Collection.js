a68834d270f7bc459e07bc0e206f46544bca2751
KnightNight <gilbenvenitz18@gmail.com>
2022-06-10 13:53:34 -0500

    meant to have stephens changes

--- client/src/pages/Collection.js
+++ client/src/pages/Collection.js
@@ -1,5 +1,5 @@
 // page to view a collection of pieces
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
@@ -30,24 +30,24 @@ function Copyright() {
   );
 }
 
-// const theme = createTheme();
+const theme = createTheme();
 
 export default function AddCollection() {
+  const [collectionData, setCollectionData] = useState(0);
+  useEffect(() => {
+    const fetchData = async (req, res) => {
+      const response = await fetch(
+        'https://nba-players.herokuapp.com/players-stats'
+      );
+      console.log('is this working');
+      const artData = await response.json();
+      setCollectionData(artData);
+    };
 
-    // const [collectionData, setCollectionData] = useState(0);
-    // useEffect(() => {
-    //     const fetchData = async (req, res) => {
-    //         const response = await fetch('https://nba-players.herokuapp.com/players-stats')
-    //         console.log("is this working")
-    //         const artData = await response.json()
-    //         setCollectionData(artData)
-    //     };
-
-    //     fetchData()
-
-    // }, [])
+    fetchData();
+  }, []);
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
@@ -76,10 +76,15 @@ export default function AddCollection() {
             >
               Album layout
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -96,33 +101,35 @@ export default function AddCollection() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             {/* {cards.map((collection) => ( */}
-              <Grid item key="" xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {collectionData.name}
-                    </Typography>
-                    <Typography>
-                      {collectionData.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+            <Grid item key="" xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  sx={{
+                    // 16:9
+                    pt: '56.25%',
+                  }}
+                  image="https://source.unsplash.com/random"
+                  alt="random"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {collectionData.name}
+                  </Typography>
+                  <Typography>{collectionData.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">View</Button>
+                  <Button size="small">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
             {/* ))} */}
           </Grid>
         </Container>
@@ -145,4 +152,4 @@ export default function AddCollection() {
       {/* End footer */}
     </ThemeProvider>
   );
-};
+}
