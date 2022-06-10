237a25b4947070298dbb4885bac142fcd437925a
HunterLM <hunter109@gmail.com>
2022-06-09 22:11:22 -0500

    working on cloudinary

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,6 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID!
-    name: String!
     username: String!
     email: String!
     password: String!
