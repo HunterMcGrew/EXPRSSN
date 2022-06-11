0d130745bb61dc173a67df833ae1c2e70b47af1e
HunterLM <hunter109@gmail.com>
2022-06-11 00:52:27 -0500

    added cloudinary package to node, edited server.js with api/upload route for upload.js page, backend is now uploading to cloudinary

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { cloudinary } = require("./utils/cloudinary");
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -22,7 +23,21 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+// post route for /upload page 
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileString = req.body.data;
+    // console.log(fileString);
+    const uploadedResponse = await cloudinary.uploader.upload(fileString, {
+      upload_preset: "project3"
+    });
+    console.log(uploadedResponse);
+  } catch (err) {
+    if (err) throw err;
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" });
+  }
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
