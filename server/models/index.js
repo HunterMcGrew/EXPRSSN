1cdf8f203350578f70fbb6c4a9e2e362e0b4857d
HunterLM <hunter109@gmail.com>
2022-06-12 22:26:11 -0500

    deleted category model

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User')
 const Piece = require('./Piece');
-const Category = require('./Category');
 const Collection = require('./Collection');
 
-module.exports = { User, Piece, Category, Collection };
+module.exports = { User, Piece, Collection };
