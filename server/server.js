c21ad042fc6162fbfc65321fe170def16289b79e
HunterLM <hunter109@gmail.com>
2022-06-13 09:34:07 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: "50mb" }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
